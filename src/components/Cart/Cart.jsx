9e88ae7e400ddadfba57d3dc8e4dda1bcd885af6
wnnow <117201813+wnnow@users.noreply.github.com>
2024-08-02 10:22:14 +0700

    feat(cart): Add delete function and conditional rendering
    - Implemented handleDelete function to remove items from the cart based
    on item id
    - Added conditional rendering to display a message when the cart is empty
    - Updated the delete button's onClick handler to correctly pass item id
    to handleDelete

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -11,35 +11,56 @@ const Cart = () => {
     setCart([]);
   }
 
-  return (
-    <div className={`container ${styles.cartContainer}`}>
-      {cart.map((item) => (
-        <li key={item.id} className={styles.itemContainer}>
-          <div>
-            <img
-              className={styles.itemImage}
-              src={item.image}
-              alt={item.title}
-            />
-          </div>
-          <div>{item.title}</div>
+  function handleDelete(itemId) {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== itemId));
+  }
 
-          <div>Quantity: {item.quantity}</div>
-          <div>{item.price} $</div>
-          <div>Total price: {item.price * item.quantity} $</div>
-          <button className={styles.deleteBtn}>Delete</button>
-        </li>
-      ))}
-      <div className={styles.totalPriceContainer}>
-        <div className={styles.totalPriceText}>Total</div>
-        <div className={styles.totalPrice}>{totalPrice} $</div>
-      </div>
-      <div className={styles.checkOutBtnContainer}>
-        <button className={styles.checkOutBtn} onClick={handleCheckOut}>
-          <Link to="/checkout">Check out</Link>
-        </button>
-      </div>
-    </div>
+  return (
+    <>
+      {cart.length === 0 && (
+        <div className={styles.emptyCartContainer}>
+          <div className={styles.emptyCartText}>Cart is empty</div>
+          <button className={styles.emptyCartBtn}>
+            <Link to={"/products"}>Would you like to buy something?</Link>
+          </button>
+        </div>
+      )}
+      {cart.length > 0 && (
+        <div className={`container ${styles.cartContainer}`}>
+          {cart.map((item) => (
+            <li key={item.id} className={styles.itemContainer}>
+              <div>
+                <img
+                  className={styles.itemImage}
+                  src={item.image}
+                  alt={item.title}
+                />
+              </div>
+              <div>{item.title}</div>
+
+              <div>Quantity: {item.quantity}</div>
+              <div>{item.price} $</div>
+              <div>Total price: {item.price * item.quantity} $</div>
+              <button
+                className={styles.deleteBtn}
+                onClick={() => handleDelete(item.id)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+          <div className={styles.totalPriceContainer}>
+            <div className={styles.totalPriceText}>Total</div>
+            <div className={styles.totalPrice}>{totalPrice} $</div>
+          </div>
+          <div className={styles.checkOutBtnContainer}>
+            <button className={styles.checkOutBtn} onClick={handleCheckOut}>
+              <Link to="/checkout">Check out</Link>
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
